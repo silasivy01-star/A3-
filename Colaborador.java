51cdc2cd17c7faaf88f272cf25050ae101c2ddd6
silasivy01-star <silas.ivy01@gmail.com>
2026-05-26 16:46:48 -0300

    Colaborador.java

---
+++ Colaborador.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Colaborador {
+
+    private String nome;
+    private String funcao;
+
+    public Colaborador(String nome, String funcao) {
+        this.nome = nome;
+        this.funcao = funcao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + " | Função: " + funcao;
+    }
+}
