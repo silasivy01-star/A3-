aab974a1875cc00519bd17797d9feec8a5f5d9a8
silasivy01-star <silas.ivy01@gmail.com>
2026-05-26 16:56:43 -0300

    Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import view.MenuView;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Inicialização do sistema
+        MenuView menu = new MenuView();
+
+        // Execução do menu principal
+        menu.iniciarSistema();
+    }
+}
